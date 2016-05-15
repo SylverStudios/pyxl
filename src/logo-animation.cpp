7deb725bd0c141a826d94e7eb0f6d1d84fa62e10
Sam Roberts <samgqroberts@gmail.com>
2016-05-15 00:25:01 -0400

    only print at untouched coordinates

--- src/logo-animation.cpp
+++ src/logo-animation.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <time.h>
 #include <stdlib.h>
 #include <ncurses.h>
@@ -21,21 +22,30 @@ int main ()
   // initialize values needed for random character printing
   int numLines = filecontents.size();
   int numCols = filecontents.at(0).size(); // FIXME assumes all lines are same length (and 1 exists)
+  int totalCoords = numLines * numCols;
+  vector<int> unprintedCoords;
+  for( int i = 0 ; i < totalCoords ; i++ ) {
+    unprintedCoords.push_back(i);
+  }
+  random_shuffle(unprintedCoords.begin(), unprintedCoords.end());
 
   // initialize curses screen
 	initscr();
+  curs_set(0);
 
   // loop through characters in text file, print them to screen at appropriate coordinates
   srand (time(NULL));
-  for (;;) {
-    int lineCoord = rand() % numLines;
-    int colCoord = rand() % numCols;
+  for ( int i = 0 ; i < totalCoords ; i++ ) {
+    int coord = unprintedCoords.at(i);
+    int lineCoord = coord / numCols;
+    int colCoord = coord % numCols;
     string line = filecontents.at(lineCoord);
     char character = line.at(colCoord);
     mvaddch(lineCoord, colCoord, character);
     refresh();
-    wait(20);
+    wait(10);
   }
+  getch();
 	endwin();			/* End curses mode		  */
 	return 0;
 }
